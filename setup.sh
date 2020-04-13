3cbea8a7d465c4784fb3e20927a16855d6bebcec
shibe23 <shibe23.info@gmail.com>
2020-04-14 00:23:02 +0900

    fix: update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
-NODE_VERSION="10.16.3"
-NPM_VERSION="6.9.0"
+NODE_VERSION="stable"
+NPM_VERSION="stable"
 
 npm init -y
 
-echo NODE_VERSION > .node-version
-echo NPM_VERSION > .npm-version
+echo $NODE_VERSION > .node-version
+echo $NPM_VERSION > .npm-version
 cat > .npmrc << EOF
 also=dev
 save-exact=true
 engine-strict=true
 EOF
 
-# npm install -D webpack webpack-cli babel-loader @babel/core @babel/preset-env
+npm install -D webpack webpack-cli babel-loader @babel/core @babel/preset-env typescript npm-run-all
+
+mkdir src
+touch src/index.ts
